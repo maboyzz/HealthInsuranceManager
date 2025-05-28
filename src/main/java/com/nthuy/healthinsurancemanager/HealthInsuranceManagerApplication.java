0b611c0308d271e6e67003a20a0ac61e647afdbc
maboyzz <nthuy311023@gmail.com>
2025-05-28 21:07:26 +0700

    first

---
+++ src/main/java/com/nthuy/healthinsurancemanager/HealthInsuranceManagerApplication.java
@@ -0,0 +1,13 @@
+package com.nthuy.healthinsurancemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthInsuranceManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthInsuranceManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nthuy/healthinsurancemanager/HealthInsuranceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nthuy.healthinsurancemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthInsuranceManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
